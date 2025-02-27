91f2fa0e5da6a134bdeea683630418da7e17539a
laporeon <175569928+daspeon@users.noreply.github.com>
2025-02-27 16:16:15 -0300

    refactor: return questions

--- src/helpers/fileReader.ts
+++ src/helpers/fileReader.ts
@@ -1,14 +1,19 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-const FILE_PATH = path.resolve(__dirname, '..', 'data', 'formulario.txt');
+const FILE_PATH = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  'src',
+  'data',
+  'formulario.txt',
+);
 
-export const readFile = async () => {
+export const getFileContent = async () => {
   const fileContent = await fs.readFile(FILE_PATH, 'utf-8');
 
   const questions = fileContent.split('\n');
 
-  for (let question of questions) {
-    console.log(question);
-  }
+  return questions;
 };
