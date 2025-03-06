fa4bc7bc77deea0a6ff19afe39b9ea4dd90cbd35
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-06 16:31:32 -0300

    refactor: rewrite validations

--- src/helpers/answerSchema.ts
+++ src/helpers/answerSchema.ts
@@ -5,13 +5,18 @@ export const answerSchema = [
     message: 'Nome não pode estar vazio e deve contar no mínimo 3 caracteres.',
   }),
   z.string().email({ message: 'Email inválido.' }),
-  z.string().refine(val => !isNaN(Number(val)) && Number(val) > 18, {
-    message: 'Idade deve ser um número e maior do que 18 anos.',
-  }),
   z
     .string()
-    .transform(val => parseFloat(val.replace(',', '.')))
-    .refine(val => val >= 0.5 && val <= 2.5, {
+    .refine(
+      age => !isNaN(Number(age)) && Number(age) >= 1 && Number(age) <= 120,
+      {
+        message: 'Idade deve ser um número inteiro entre 01 e 120',
+      },
+    ),
+  z
+    .string()
+    .transform(height => parseFloat(height.replace(',', '.')))
+    .refine(height => height >= 0.5 && height <= 2.5, {
       message: 'Altura deve ser digitada no formato 1,85',
     }),
 ];
