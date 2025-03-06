6243df4b8049a813e3f1b3b7699eee662ef7af0e
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-06 20:14:44 -0300

    refactor: remove unused constructor args

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
-import { FilesController } from './controllers/files.controller';
-import { QuestionController } from './controllers/questions.controller';
-import { UserController } from './controllers/users.controller';
 import { MainMenu } from './helpers/menu';
 
-const filesController = new FilesController();
-const userController = new UserController(filesController);
-const questionController = new QuestionController(
-  userController,
-  filesController,
-);
-const menu = new MainMenu(userController, questionController);
+const menu = new MainMenu();
 
 menu.display();
