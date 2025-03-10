3776051250a99f8ca3f159905c5c397bd804a2f0
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-10 16:34:56 -0300

    feat: create file with reusable constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,7 @@
+import path from 'node:path';
+
+const filePath = path.resolve(__dirname, '../../src/data/formulario.txt');
+
+const folderPath = path.resolve(__dirname, '../../src/data/users');
+
+export { filePath, folderPath };
