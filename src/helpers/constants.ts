12a7649981e7a7007264a7bf6ef5960af3f9cade
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-11 13:55:59 -0300

    refactor: remove readline file and add readline init to constants file

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,7 +1,13 @@
 import path from 'node:path';
+import readline from 'node:readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const filePath = path.resolve(__dirname, '../../src/data/formulario.txt');
 
 const folderPath = path.resolve(__dirname, '../../src/data/users');
 
-export { filePath, folderPath };
+export { filePath, folderPath, rl };
