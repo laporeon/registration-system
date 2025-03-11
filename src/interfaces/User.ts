9df5a3693e0404134cc7c65b282a8f12d778cb5d
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-11 20:35:13 -0300

    feat: add optional data array to store answers from new questions

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -3,4 +3,5 @@ export interface User {
   email: string;
   age: string;
   height: string;
+  data?: string[]; // Since users can add new questions, we need this optional attribute to handle them
 }
