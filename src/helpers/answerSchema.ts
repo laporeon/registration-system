439a90cd160bfd06aa8c7fbd3062a889c5bf31fb
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-11 20:37:49 -0300

    feat: create schema to validate answers from new questions

--- src/helpers/answerSchema.ts
+++ src/helpers/answerSchema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
-export const answerSchema = [
+const answerSchema = [
   z.string().min(3, {
-    message: 'Nome não pode estar vazio e deve contar no mínimo 3 caracteres.',
+    message: 'Nome não pode estar vazio e deve conter no mínimo 3 caracteres.',
   }),
   z.string().email({ message: 'Email inválido.' }),
   z
@@ -10,13 +10,22 @@ export const answerSchema = [
     .refine(
       age => !isNaN(Number(age)) && Number(age) >= 1 && Number(age) <= 120,
       {
-        message: 'Idade deve ser um número inteiro entre 01 e 120',
+        message: 'Idade deve ser um número inteiro entre 01 e 120.',
       },
     ),
   z
     .string()
     .transform(height => parseFloat(height.replace(',', '.')))
     .refine(height => height >= 0.5 && height <= 2.5, {
-      message: 'Altura deve ser digitada no formato 1,85',
+      message: 'Altura deve ser digitada no formato 1,85.',
     }),
 ];
+
+const dynamicAnswerSchema = z
+  .string()
+  .min(1, {
+    message: 'Resposta não pode estar vazia.',
+  })
+  .optional();
+
+export { answerSchema, dynamicAnswerSchema };
