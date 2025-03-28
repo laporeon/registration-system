2d9d5c028440f5a7ee4118b242a05dd2a26f30d0
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-28 14:01:50 -0300

    feat: import express and create test home route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { MainMenu } from '@helpers/menu';
+import express, { Request, Response } from 'express';
 
-const menu = new MainMenu();
+import 'dotenv/config';
+import env from '@config/env';
 
-menu.display();
+const app = express();
+
+const PORT = env.port || 3000;
+console.log(PORT);
+
+app.use(express.json());
+
+app.get('/', (_: Request, res: Response) => {
+  res.json({ message: 'Hello!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at: http://localhost:${PORT}`);
+});
