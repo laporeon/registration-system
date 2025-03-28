26e5da8b84eb3b86de0a471311a9e5775ebbdaca
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-28 18:37:05 -0300

    feat: import and use user routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from 'express';
 
-import 'dotenv/config';
-import env from '@config/env';
+import { env } from '@config/env';
+import { userRoutes } from '@routes/user.routes';
 
 const app = express();
 
 const PORT = env.port || 3000;
-console.log(PORT);
 
 app.use(express.json());
+
+app.use('/users', userRoutes);
 
 app.get('/', (_: Request, res: Response) => {
   res.json({ message: 'Hello!' });
