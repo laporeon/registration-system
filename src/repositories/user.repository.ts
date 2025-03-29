e3b8758c801a0607a72438d1843a8c5b52500709
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-29 18:48:28 -0300

    refactor: remove connectToCollection method and import it from Mongo file

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,32 +1,22 @@
-import { connectToMongoDB, disconnectMongoDB } from '@database/mongo';
+import { getCollection } from '@database/mongo';
 import { User } from '@interfaces/User';
 
 export class UserRepository {
-  async connectToCollection() {
-    const database = await connectToMongoDB();
-    const collection = await database.collection<User>('users');
-    return collection;
-  }
-
   async create(userData: User) {
-    const collection = await this.connectToCollection();
+    const collection = await getCollection();
 
     const user = await collection.insertOne({
       ...userData,
       createdAt: new Date(),
     });
 
-    await disconnectMongoDB();
-
     return user;
   }
 
   async list() {
-    const collection = await this.connectToCollection();
+    const collection = await getCollection();
 
     const users = await collection.find().toArray();
-
-    await disconnectMongoDB();
 
     return users;
   }
