773732b135d1fdbc1756054306d881f86b6ecd93
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-31 17:15:35 -0300

    chore: remove try catch

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,25 +6,17 @@ import { UserService } from '@services/user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  async create(request: Request, response: Response): Promise<any> {
-    try {
-      const user = await this.userService.create(request.body);
-      return response.status(HTTPStatus.CREATED).json(user);
-    } catch (error) {
-      return response.status(HTTPStatus.INTERNAL_SERVER_ERROR).json(error);
-    }
+  async create(request: Request, response: Response): Promise<Response> {
+    const user = await this.userService.create(request.body);
+    return response.status(HTTPStatus.CREATED).json(user);
   }
 
-  async list(request: Request, response: Response): Promise<any> {
-    try {
-      const users = await this.userService.list(request.params?.id);
-      return response.status(HTTPStatus.OK).json(users);
-    } catch (error) {
-      return response.status(HTTPStatus.INTERNAL_SERVER_ERROR).json(error);
-    }
+  async list(request: Request, response: Response): Promise<Response> {
+    const users = await this.userService.list(request.params?.id);
+    return response.status(HTTPStatus.OK).json(users);
   }
 
-  async delete(request: Request, response: Response): Promise<any> {
+  async delete(request: Request, response: Response): Promise<Response> {
     await this.userService.delete(request.params.id);
     return response.status(HTTPStatus.NO_CONTENT).json();
   }
