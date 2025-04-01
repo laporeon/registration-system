d192729dc86157372bf76cfc15e22fe8650d09ec
laporeon <175569928+daspeon@users.noreply.github.com>
2025-04-01 17:06:31 -0300

    feat: import swagger route and remove unused home route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import 'express-async-errors';
 
 import { env } from '@/config/env';
 import { logger } from '@/helpers';
 import { errorHandler } from '@/middlewares';
-import { userRoutes } from '@/routes/user.routes';
+import { userRoutes, swaggerRoutes } from '@/routes';
 
 const app = express();
 
@@ -13,10 +13,7 @@ const PORT = env.port || 3000;
 app.use(express.json());
 
 app.use('/users', userRoutes);
-
-app.get('/', (_: Request, res: Response) => {
-  res.json({ message: 'Hello!' });
-});
+app.use('/api/docs', swaggerRoutes);
 
 app.use(errorHandler);
 
