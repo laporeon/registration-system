db80acce5bb682bf2a695a4801a2aa6e69db47bf
laporeon <175569928+daspeon@users.noreply.github.com>
2025-04-03 16:10:30 -0300

    chore: improve database connection

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,4 +1,4 @@
-import { Collection, Db, MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 
 import { env } from '@/config/env';
 import { logger } from '@/helpers';
@@ -7,25 +7,28 @@ const mongoUri = `mongodb://${env.username}:${env.password}@${env.host}:27017/${
 
 const client = new MongoClient(mongoUri, {
   timeoutMS: 5000,
+  connectTimeoutMS: 5000,
 });
 
-// TODO: refactor connection
-async function connect(): Promise<any> {
+let database: Db | null = null;
+
+async function connect(): Promise<Db> {
+  if (database) return database;
+
   try {
     await client.connect();
-    const database: Db = client.db(env.database);
-    const collection: Collection = await database.collection('users');
-    logger.info('Connected to dabatase.');
-    return { database, collection };
+    database = client.db();
+    logger.info('✅️ Succesfully connected to dabatase!');
+    return database;
   } catch (error) {
     console.log({ error });
-    logger.error(`Database connection failed.`, error);
+    logger.error(`❌ Database connection failed.`, error);
     throw new Error('Failed to connect to database.');
   }
 }
 
 async function disconnect() {
-  await client.close();
+  if (database) await client.close();
   logger.info('Connection with database closed!');
 }
 
