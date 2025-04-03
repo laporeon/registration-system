77bb99e3cf06819425447f841dba1bd2467d2838
laporeon <175569928+daspeon@users.noreply.github.com>
2025-04-03 15:06:55 -0300

    feat: implement update method and return user data at create method

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -8,12 +8,16 @@ export class UserRepository {
   async create(userData: User): Promise<any> {
     const { collection } = await connect();
 
-    const user = await collection.insertOne({
+    const { insertedId } = await collection.insertOne({
       ...userData,
       createdAt: new Date(),
     });
 
-    return user;
+    // Since MongoDB itself doesn't return full object inserted data one insertOne, we need to do it manually
+    return {
+      _id: insertedId,
+      ...userData,
+    };
   }
 
   async list(id?: string): Promise<any> {
@@ -30,6 +34,24 @@ export class UserRepository {
     if (data.length <= 0) throw new NotFoundError();
 
     return data;
+  }
+
+  async update(id: string, fieldsToUpdate: Partial<User>): Promise<any> {
+    const { collection } = await connect();
+
+    if (!ObjectId.isValid(id)) throw new InvalidRequiredField();
+
+    const user = await collection.findOneAndUpdate(
+      {
+        _id: new ObjectId(id),
+      },
+      { $set: { ...fieldsToUpdate, updatedAt: new Date() } },
+      { returnDocument: 'after' },
+    );
+
+    if (!user) throw new NotFoundError();
+
+    return user;
   }
 
   async delete(id: string): Promise<void> {
