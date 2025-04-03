68cbc08f53106f8b48e2a03ff85c8aee42828caa
laporeon <175569928+daspeon@users.noreply.github.com>
2025-04-03 15:07:26 -0300

    feat: implement update service method

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -12,6 +12,10 @@ export class UserService {
     return await this.userRepository.list(id);
   }
 
+  async update(id: string, fieldsToUpdate: Partial<User>): Promise<any> {
+    return await this.userRepository.update(id, fieldsToUpdate);
+  }
+
   async delete(id: string): Promise<void> {
     await this.userRepository.delete(id);
   }
