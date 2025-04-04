2024752faae545f76aee68037707f1ee583a6b05
laporeon <175569928+daspeon@users.noreply.github.com>
2025-04-04 15:46:45 -0300

    refactor: change how application port is declared

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 
 export const env = {
-  port: process.env.PORT ?? 3000,
-  host: process.env.MONGO_HOST ?? 'localhost',
-  username: process.env.MONGO_USER ?? '',
-  password: process.env.MONGO_PASSWORD ?? '',
-  database: process.env.MONGO_DB ?? '',
+  port: process.env.PORT || 3001,
+  host: process.env.MONGO_HOST || 'localhost',
+  username: process.env.MONGO_USER || 'root',
+  password: process.env.MONGO_PASSWORD || 'password',
+  database: process.env.MONGO_DB || 'wiredcraft',
 };
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import 'dotenv/config';
 
+import { env } from '@/config/env';
 import { logger } from '@/helpers';
 import { errorHandler } from '@/middlewares';
 import { userRoutes, swaggerRoutes } from '@/routes';
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = env.port;
 
 app.use(express.json());
 
