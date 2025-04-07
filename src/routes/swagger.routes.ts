6c2e1659965c59096f995c6c6a39758e1695be28
laporeon <175569928+daspeon@users.noreply.github.com>
2025-04-07 17:20:11 -0300

    refactor: update swagger import

--- src/routes/swagger.routes.ts
+++ src/routes/swagger.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import { serve, setup } from 'swagger-ui-express';
 
-import swaggerConfig from '@/docs';
+import { swagger } from '@/docs/swagger';
 
 const routes = Router();
 
-routes.use('/', serve, setup(swaggerConfig));
+const options = {
+  swaggerOptions: {
+    defaultModelsExpandDepth: -1,
+    defaultModelExpandDepth: -1,
+  },
+};
+
+routes.use('/', serve, setup(swagger, options));
 
 export { routes as swaggerRoutes };
